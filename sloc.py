880dda7b1eb47276ac16f91eb134ce6774f7a8bf
swqd <longlance21@gmail.com>
2021-04-19 11:31:00 +0300

    Add option to show detailed output

--- sloc.py
+++ sloc.py
@@ -6,25 +6,29 @@ def main():
     parser.add_argument("location", help="Location to calculate SLOC count in")
     parser.add_argument("--fe", '--files-exclude', nargs="+", help="File types to exclude")
     parser.add_argument("--de", '--dir-exclude', nargs="+", help="Directories to exclude")
-    parser.add_argument("--debug", help="Show debug info")
+    parser.add_argument("--d", action='store_true', help="Show detailed output")
+    parser.add_argument("--debug", action='store_true', help="Show debug info")
     args = parser.parse_args()
 
     excluded_files = []
     excluded_directories = []
-    if (args.fe):
+    if args.fe:
         excluded_files = args.fe
 
-    if (args.de):
+    if args.de:
         excluded_directories = args.de
 
-    if (args.debug):
+    if args.debug:
         print("Target location:", args.location)
         print("File type excluded:", args.fe)
         print("Directories excluded:", args.de)
 
     try:
         sloc_calculator = SlocCalculator.SlocCalculator(args.location, excluded_files, excluded_directories)
-        sloc_calculator.get_detailed_report()
+        if (args.d):
+            sloc_calculator.get_detailed_report()
+        else:
+            sloc_calculator.get_base_report()
     except Exception as e:
         print(str(e))
 
